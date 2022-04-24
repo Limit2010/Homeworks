292ec182c8453e97502944564e7bc4f6f72a921a
Limit2010 <Limit2010@inbox.ru>
2022-04-24 16:11:26 +0700

    Initial commit

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson2
+{
+   public class Program
+    {
+       public static void Main ()
+        {
+            Console.WriteLine("Hello Word");
+            Console.ReadLine();
+        }
+    }
+}
