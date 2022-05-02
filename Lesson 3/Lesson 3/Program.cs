e7b5c581e3a413c79205b2e2b762bc61dcc5c6cd
Limit2010 <Limit2010@inbox.ru>
2022-05-02 15:53:50 +0700

    Data entered in one line. Exceptions considered

--- Lesson 3/Lesson 3/Program.cs
+++ Lesson 3/Lesson 3/Program.cs
@@ -7,109 +7,212 @@
         {
             while (true)
             {
-                Console.WriteLine("Enter the first value");
-                long.TryParse(Console.ReadLine(), out var value1);
-                if (value1 == 0)
-                {
-                    Console.WriteLine("Eror. Please use integers from -9*10^18 to 9*10^18");
-                    Console.WriteLine();
-                    continue;                   
-                }
+                var actionList = new char[] { '+', '*', '/', '%', '-' };
+                Console.WriteLine("Введите выражение из двух чисел.\n" + "(Если в выражении есть отрицательное число, то его необходимо вводить первым)");
+                var expression = Console.ReadLine();
+                var action = ' ';
+                long value1;
+                long value2;
+                var Check = new string [5];
+                string Negative;
 
-                Console.WriteLine("Enter math expression");
-                var action = Console.ReadLine();
-                if (action != "+" & action != "-" & action != "*" & action != "/" & action != "%")
+                for (int i = 0; i < actionList.Length; i++)
                 {
-                    Console.WriteLine("Eror. Please use one of the Math action (+ - * / %)");
-                    Console.WriteLine();
+                    var poisk = expression.IndexOf(actionList[i]);
+                    if (poisk > -1)
+                    {
+                        action = actionList[i];
+                        //Console.WriteLine("Действие: " + action + "\n");                        
+                        break;
+                    }
+                    else if (i == actionList.Length - 1)
+                    {
+                        Console.WriteLine("Ошибка! Ожидалось математическое действие (+ - * / %).");
+                    }
+                }
+                // Console.WriteLine("Действие: " + action + "\n");
+
+                var array = expression.Split(action);                
+
+                if (action == '-')
+                {
+                    int.TryParse(array[0], out var GdeMinus);
+                   // Console.WriteLine("Где минус: " + GdeMinus);
+
+                   if (GdeMinus == 0)
+                    {
+                        array[0] = ("-" + array[1]);
+                        array[1] = array[2];
+
+                        if (array.Length > 3)
+                        {
+                            Console.WriteLine("Вы пытаетесь выполнить более одного математического действия\n");
+                            continue;
+                        }                    
+                    }
+                   else
+                    {
+                        if (array.Length > 2)
+                        {
+                            Console.WriteLine("Вы пытаетесь выполнить более одного математического действия\n");
+                            continue;
+                        }
+                    }
+                  
+                }
+                else
+                {
+                    Negative = array[0];
+
+                    if (array.Length > 2)
+                    {
+                        Console.WriteLine("Вы пытаетесь выполнить более одного математического действия\n");
+                        continue;
+                    }
+
+                    if (Negative.StartsWith("-"))
+                    {
+                        for (int i = 1; i < array.Length; i++)
+                        {
+                            var CheckActionNumber = array[i];
+                            Check = CheckActionNumber.Split(actionList);
+                            if (Check.Length > 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            var CheckActionNumber = array[i];
+                            Check = CheckActionNumber.Split(actionList);
+                            if (Check.Length > 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (Check.Length > 1)
+                    {
+                        Console.WriteLine("Вы пытаетесь выполнить более одного математического действия\n");
+                        continue;
+                    }
+                }           
+
+                try
+                 {
+                     value1 = long.Parse(array[0]);
+                     value2 = long.Parse(array[1]);                      
+                 }
+                 catch (FormatException)
+                 {
+                     Console.WriteLine("Ошибка! Используйте целые числа в диапазоне от -9*10^18 до 9*10^18\n");
+                     continue;
+                 }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка! Одно из значений не входит в диапазон от -9*10^18 до 9*10^18\n");
                     continue;
                 }
-
-                Console.WriteLine("Enter the second value");
-                long.TryParse(Console.ReadLine(), out var value2);
-                if (value2 == 0)
+                catch (IndexOutOfRangeException)
                 {
-                    Console.WriteLine("Eror. Please use integers from -9*10^18 to 9*10^18");
-                    Console.WriteLine();
+                    Console.WriteLine("Необходимо ввести выражение из двух чисел. Например, 5+5\n");
                     continue;
                 }
-
-
-
-                switch (action)
-                {
-                    case "+":
-                        long result = Summ(value1, value2);
-                        if (result > 9000000000000000000)
-                        {
-                            Console.WriteLine("The result is out of range [-9*10^18 to 9*10^18]");
-                            Console.WriteLine();
-                            continue;
-                        }
-                        Console.WriteLine("result: " + result);
-                        Console.WriteLine();                                            
-                        break;
-                    case "-":
-                        long result1 = difference(value1, value2);
-                        if (result1 < -9000000000000000000)
-                        {
-                            Console.WriteLine("The result is out of range [-9*10^18 to 9*10^18]");
-                            Console.WriteLine();
-                            continue;
-                        }
-                        Console.WriteLine("result: " + result1);
-                        Console.WriteLine();
-                        break;
-                    case "*":
-                        long result2 = multiplication(value1, value2);
-                        if (result2 > 9000000000000000000)
-                        {
-                            Console.WriteLine("The result is out of range [-9*10^18 to 9*10^18]");
-                            Console.WriteLine();
-                            continue;
-                        }
-                        else if (result2 < -9000000000000000000)
-                        {
-                            Console.WriteLine("The result is out of range [-9*10^18 to 9*10^18]");
-                            Console.WriteLine();
-                            continue;
-                        }
-                        Console.WriteLine("result: " + result2);
-                        Console.WriteLine();
-                        break;
-                    case "/":
-                        long result3 = Divide(value1, value2);
-                        Console.WriteLine("result: " + result3);
-                        Console.WriteLine();
-                        break;
-                    case "%":
-                        long result4 = RemainderOfDivision(value1, value2);
-                        Console.WriteLine("result: " + result4);
-                        Console.WriteLine();
-                        break;
-
-                }
+                
+                    Switch(action, value1, value2);
             }
         }
+
+        public static void Switch(char action, long value1, long value2)
+        {
+            switch (action)
+            {
+                case '+':
+                    long result = Summ(value1, value2);                   
+                    Console.WriteLine("Результат: " + result+ "\n");                   
+                    break;
+                case '-':
+                    long result1 = Difference(value1, value2);                   
+                    Console.WriteLine("Результат: " + result1+ "\n");                 
+                    break;
+                case '*':
+                    long result2 = Multiplication(value1, value2);                      
+                    Console.WriteLine("Результат: " + result2 + "\n");                    
+                    break;
+                case '/':
+                    long result3 = Divide(value1, value2);
+                    if (value2 > 0)
+                    {
+                        Console.WriteLine("Результат: " + result3 + "\n");
+                    }                                       
+                    break;
+                case '%':
+                    long result4 = RemainderOfDivision(value1, value2);
+                    Console.WriteLine("Результат: " + result4 + "\n");                    
+                    break;
+                case ' ':
+                    Console.WriteLine();
+                    break;
+            }
+        } 
         public static long Summ(long value1, long value2)
         {
-            long summ = value1 + value2;
-            return summ;
-            
+            long summ=0;
+            try
+            {
+                summ = checked(value1 + value2);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка! Результат находится за пределами диапазона [-9*10^18 ; 9*10^18]\n");
+                Main();
+            }
+            return summ;            
         }
-        public static long difference(long value1, long value2)
+        public static long Difference(long value1, long value2)
         {
-            long difference = value1 - value2;
+            long difference = 0;
+            try
+            {
+                difference = checked(value1 - value2);
+            }
+           catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка! Результат находится за пределами диапазона [-9*10^18 ; 9*10^18]\n");
+                Main();
+            }
             return difference;
         }
-        public static long multiplication(long value1, long value2)
+        public static long Multiplication(long value1, long value2)
         {
-            long multiplication = value1 * value2;
+            long multiplication = 0;            
+            try 
+            {                
+                multiplication = checked(value1 * value2);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка! Результат находится за пределами диапазона [-9*10^18 ; 9*10^18]\n");
+                Main();
+            }
             return multiplication;
         }
         public static long Divide(long value1, long value2)
         {
-            long Divide = value1 / value2;
+            long Divide = 0;
+            try
+            {
+                Divide = value1 / value2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка! На ноль делить нельзя!\n");
+                Main();
+            }
             return Divide;
         }
         public static long RemainderOfDivision(long value1, long value2)
@@ -118,6 +221,5 @@
             return RemainderOfDivision;
         }
     }
-
 }
 
